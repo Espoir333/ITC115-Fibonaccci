c94929c32dc26d11eb47bce54adedcad1e7a5dcf
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2019-10-07 16:27:03 -0700

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,18 @@
+public static void fibonacci(int number) {
+    
+        int fibo1 = 1; 
+        int fibo2 = 1; 
+        System.out.printf("%nFibonacci series of %d numbers are : ", number); 
+        System.out.printf("%s ", fibo1); 
+        System.out.printf("%s ", fibo2);
+    
+        for (int i = 2; i &lt; number; i++) { 
+            int fibonacci = fibo1 + fibo2;
+            System.out.printf("%s ", fibonacci); 
+            fibo2 = fibo1;
+            fibo1 = fibonacci; } }
+
+Read more: https://javarevisited.blogspot.com/2015/01/print-fibonacci-series-in-java-using.html#ixzz61iI0wnsY
 import java.util.Arrays;
 import java.util.Scanner;
 
